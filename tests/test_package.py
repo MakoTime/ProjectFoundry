2cd289b364c6245612662b0c6e9777e31d78c376
Ben <ben.vernon.bosley@gmail.com>
2026-09-04 12:12:43 +1200

    Updated package test to move away from static version

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,8 +1,10 @@
+import re
+
 from projectfoundry import __version__, update_instructions
 
 
 def test_package_version() -> None:
-    assert __version__ == "0.1.0"
+    assert re.fullmatch(r"\d+\.\d+\.\d+", __version__)
 
 
 def test_update_instructions_is_available_from_package_root(tmp_path) -> None:
